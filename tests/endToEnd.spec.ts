46b10270225ed0f1b32ab2a37deb449a15f1b4aa
Eduardo Marquez <eduardo.marquez.isc@gmail.com>
2025-07-02 16:03:12 -0600

    handling calendar

--- tests/endToEnd.spec.ts
+++ tests/endToEnd.spec.ts
@@ -24,7 +24,7 @@ test("End to end framework", async ({ page }) => {
     const options = page.locator(".ta-results");
     await expect(options).toBeVisible();
     await options.locator("button:has-text('Japan')").click();
-    expect( await page.locator(".user__name [type='text']").first()).toHaveText(email);
+    expect(await page.locator(".user__name [type='text']").first()).toHaveText(email);
     await page.locator(".action__submit").click();
     await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
     const pageOrderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
@@ -32,13 +32,36 @@ test("End to end framework", async ({ page }) => {
     await page.locator("[routerlink*='myorder']").click();
     await page.locator("tbody").waitFor();
     const rows = await page.locator("tbody tr").all();
-    for(const row of rows){
+    for (const row of rows) {
         const rowOrderID = await row.locator("th").textContent();
-        if(rowOrderID.includes(OrderID)){
+        if (rowOrderID.includes(OrderID)) {
             await row.locator("button.btn-primary").click();
             break;
         }
-    const orderIDDetails = await page.locator(".col-text").textContent();
-    expect(orderIDDetails.includes(OrderID)).toBeTruthy();
+        const orderIDDetails = await page.locator(".col-text").textContent();
+        expect(orderIDDetails.includes(OrderID)).toBeTruthy();
     }
+})
+
+test("Client app login", async ({ page }) => {
+    const produtToBuy = "ADIDAS ORIGINAL";
+    const email = "anshika@gmail.com";
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.getByPlaceholder("email@example.com").fill(email);
+    await page.getByPlaceholder("enter your passsword").fill("Iamking@000");
+    await page.getByRole("button", { name: "login" }).click();
+    await page.waitForLoadState("networkidle");
+    await page.locator(".card-body b").first().waitFor();
+    await page.locator(".card-body").filter({ hasText: produtToBuy }).getByRole("button", { name: "Add To Cart" }).click();
+    await page.getByRole("listitem").getByRole("button", { name: "Cart" }).click();
+    await page.locator("div li").first().waitFor();
+    await expect(page.getByText(produtToBuy)).toBeVisible();
+    await page.getByRole("button", { name: "Checkout" }).click();
+    await page.getByPlaceholder("Select Country").pressSequentially("ja");
+    const options = page.locator(".ta-results");
+    await expect(options).toBeVisible();
+    await options.locator("button:has-text('Japan')").click();
+    expect(await page.getByText("email")).toBeVisible();
+    await page.getByText("PLACE ORDER").click();
+    await expect(page.getByText(" Thankyou for the order. ")).toBeVisible();
 })
--- tests/getByLocators.spec.ts
+++ tests/getByLocators.spec.ts
@@ -1,14 +1,32 @@
-import test from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("Understand GetBy", async ({ page}) =>{
+test("Understand GetBy", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel("Check me out if you Love IceCreams!").click();
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").selectOption("Female");
     //npx playwright test --ui
     await page.getByPlaceholder("Password").fill("password");
-    await page.getByRole("button",{name:"Submit"}).click();
+    await page.getByRole("button", { name: "Submit" }).click();
     await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
-    await page.getByRole("link",{name: "Shop"}).click();
-    await page.locator("app-card").filter({hasText: "Nokia Edge"}).getByRole("button").click();
+    await page.getByRole("link", { name: "Shop" }).click();
+    await page.locator("app-card").filter({ hasText: "Nokia Edge" }).getByRole("button").click();
+})
+
+test("Handling calendars", async ({ page }) => {
+    const month = "5";
+    const day = "15";
+    const year = "2028";
+    await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+    await page.locator(".react-date-picker__inputGroup").click();
+    await page.locator(".react-calendar__navigation__label").click();
+    await page.locator(".react-calendar__navigation__label").click();
+    await page.getByText(year).click();
+    await page.locator(".react-calendar__year-view__months__month").nth(Number(month) - 1).click();
+    await page.locator("//abbr[text()='" + day + "']").click();
+    const inputs = await page.locator(".react-date-picker__inputGroup input").all();
+    const values = await Promise.all(inputs.map(input => input.getAttribute("value")));
+    expect(values).toContain(month);
+    expect(values).toContain(day);
+    expect(values).toContain(year);
 })
